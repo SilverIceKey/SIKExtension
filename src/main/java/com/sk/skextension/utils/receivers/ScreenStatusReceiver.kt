e03366cd9e5abd514aafa480c4aa5f1815b93be3
SilverIceKey <z516798599@gmail.com>
2021-12-14 09:19:46 +0800

    change class name

--- src/main/java/com/sk/skextension/utils/receivers/ScreenStatusReceiver.kt
+++ src/main/java/com/sk/skextension/utils/receivers/ScreenStatusReceiver.kt
@@ -4,12 +4,18 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import com.sk.skextension.utils.eventbus.BusModel
+import com.sk.skextension.utils.net.mqtt.EMQXHelper
+import org.greenrobot.eventbus.EventBus
 
-class LockScreenReceiver:BroadcastReceiver() {
+/**
+ * 显示器状态监听
+ */
+class ScreenStatusReceiver:BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         val action = intent!!.action
         if (action == Intent.ACTION_SCREEN_ON) {
-
+            EventBus.getDefault().post(BusModel(EMQXHelper.EVENTBUS_TYPE,EMQXHelper.SCREEN_ON))
         } else if (action == Intent.ACTION_SCREEN_OFF) {
 
         }
@@ -20,7 +26,7 @@ class LockScreenReceiver:BroadcastReceiver() {
             filter.addAction(Intent.ACTION_SCREEN_ON)
             filter.addAction(Intent.ACTION_SCREEN_OFF)
             filter.addAction(Intent.ACTION_USER_PRESENT)
-            context?.registerReceiver(LockScreenReceiver(), filter)
+            context?.registerReceiver(ScreenStatusReceiver(), filter)
         }
     }
 }
