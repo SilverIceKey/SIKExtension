c4f4fe8c88eb4ecca31402ebcdab4a03e8a80ab2
SilverIceKey <z516798599@gmail.com>
2022-02-22 17:00:49 +0800

    新增扩展

--- src/main/java/com/sk/skextension/kotlin/ArrayExtension.kt
+++ src/main/java/com/sk/skextension/kotlin/ArrayExtension.kt
@@ -8,3 +8,14 @@ import java.util.*
 fun <T : Comparable<T>> Array<out T>.getString(): String {
     return Arrays.toString(this)
 }
+
+/**
+ * kotlin list转mutablelist
+ */
+fun <T:Comparable<T>> List<out T>.toMutableList():MutableList<T>{
+    val resultMutableList = mutableListOf<T>()
+    this.stream().forEach {
+        resultMutableList.add(it)
+    }
+    return resultMutableList
+}
