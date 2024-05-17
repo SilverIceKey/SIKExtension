32d058ebfc9a9aa93e16db23d85dac8d3e6c5506
SilverIceKey <z516798599@gmail.com>
2024-05-17 15:48:23 +0800

    添加全局可配置的网络请求拦截器

--- SIKNet/src/main/java/com/sik/siknet/http/HttpUtils.kt
+++ SIKNet/src/main/java/com/sik/siknet/http/HttpUtils.kt
@@ -3,6 +3,7 @@ package com.sik.siknet.http
 import com.sik.siknet.http.interceptor.AutoSaveCookieJar
 import com.sik.siknet.http.interceptor.DefaultHeaderInterceptor
 import com.sik.siknet.http.interceptor.DefaultParameterInterceptor
+import okhttp3.Interceptor
 import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
@@ -11,6 +12,16 @@ import java.util.concurrent.TimeUnit
 object HttpUtils {
 
     val CLIENT_MEDIA_TYPE: MediaType? = "application/json; charset=utf-8".toMediaTypeOrNull()
+
+    /**
+     * 拦截器
+     */
+    val interceptor: MutableList<Interceptor> = mutableListOf()
+
+    /**
+     * 网络拦截器
+     */
+    val networkInterceptor: MutableList<Interceptor> = mutableListOf()
 
     /**
      * Create ok http client
@@ -42,6 +53,18 @@ object HttpUtils {
             .cookieJar(AutoSaveCookieJar())
             .addInterceptor(DefaultHeaderInterceptor())
             .addInterceptor(DefaultParameterInterceptor())
+            .apply {
+                if (interceptor.isNotEmpty()) {
+                    interceptor.forEach {
+                        addInterceptor(it)
+                    }
+                }
+                if (networkInterceptor.isNotEmpty()) {
+                    networkInterceptor.forEach {
+                        addNetworkInterceptor(it)
+                    }
+                }
+            }
             .connectTimeout(timeoutTime, timeoutUnit)
             .readTimeout(timeoutTime, timeoutUnit)
             .writeTimeout(timeoutTime, timeoutUnit)
