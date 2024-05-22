789a827261aa186dce89ec3d97ed0cd20628dfbb
SilverIceKey <z516798599@gmail.com>
2024-05-22 22:00:03 +0800

    线程工具更新

--- SIKCore/src/main/java/com/sik/sikcore/thread/ThreadUtils.kt
+++ SIKCore/src/main/java/com/sik/sikcore/thread/ThreadUtils.kt
@@ -2,27 +2,21 @@ package com.sik.sikcore.thread
 
 import android.os.Handler
 import android.os.Looper
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.*
 
 /**
  * 线程工具
  */
 object ThreadUtils {
     /**
-     * 主线程
+     * 主线程Handler
      */
     private val mainHandler by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
         Handler(Looper.getMainLooper())
     }
 
     /**
-     * 主线程
+     * 获取主线程Handler
      */
     @JvmStatic
     fun mainHandler(): Handler {
@@ -30,126 +24,74 @@ object ThreadUtils {
     }
 
     /**
-     * Custom scope
-     * 自定义作用域
-     * @property context
-     * @constructor Create empty Custom scope
-     */
-    private class CustomScope(private val context: CoroutineContext) : CoroutineScope {
-        private var job = Job()
-        override val coroutineContext: CoroutineContext
-            get() = context + job
-
-        fun cancel() {
-            job.cancel()
-        }
-    }
-
-    /**
-     * Io scope
-     * io作用域
-     */
-    private val ioScope = CustomScope(Dispatchers.IO)
-
-    /**
-     * Main scope
-     * 主线程作用域
-     */
-    private val mainScope = CustomScope(Dispatchers.Main)
-
-    /**
-     * Run on i o
-     * 运行在io线程
+     * 运行在IO线程
      * @param block
-     * @receiver
      */
     @JvmStatic
     fun runOnIO(block: suspend CoroutineScope.() -> Unit) {
-        ioScope.launch {
+        GlobalScope.launch(Dispatchers.IO) {
             block()
         }
     }
 
     /**
-     * Run on main
      * 运行在主线程
      * @param block
-     * @receiver
      */
     @JvmStatic
     fun runOnMain(block: suspend CoroutineScope.() -> Unit) {
-        mainScope.launch {
+        GlobalScope.launch(Dispatchers.Main) {
             block()
         }
     }
 
     /**
-     * Run on i o delayed
-     * 运行在io线程并且延迟
+     * 运行在IO线程并延迟执行
      * @param delayTimeMillis
      * @param block
-     * @receiver
      */
     @JvmStatic
     fun runOnIODelayed(delayTimeMillis: Long, block: suspend CoroutineScope.() -> Unit) {
-        ioScope.launch {
+        GlobalScope.launch(Dispatchers.IO) {
             delay(delayTimeMillis)
             block()
         }
     }
 
     /**
-     * Run on main delayed
-     * 运行在主线程并且延迟
+     * 运行在主线程并延迟执行
      * @param delayTimeMillis
      * @param block
-     * @receiver
      */
     @JvmStatic
     fun runOnMainDelayed(delayTimeMillis: Long, block: suspend CoroutineScope.() -> Unit) {
-        mainScope.launch {
+        GlobalScope.launch(Dispatchers.Main) {
             delay(delayTimeMillis)
             block()
         }
     }
 
     /**
-     * With i o
-     * 切换到io线程
-     * @param T
+     * 在IO线程中运行并返回结果
      * @param block
-     * @receiver
      * @return
      */
     @JvmStatic
     suspend fun <T> withIO(block: suspend CoroutineScope.() -> T): T {
-        return withContext(ioScope.coroutineContext) {
+        return withContext(Dispatchers.IO) {
             block()
         }
     }
 
     /**
-     * With main
-     * 切换到主线程
-     * @param T
+     * 在主线程中运行并返回结果
      * @param block
-     * @receiver
      * @return
      */
     @JvmStatic
     suspend fun <T> withMain(block: suspend CoroutineScope.() -> T): T {
-        return withContext(mainScope.coroutineContext) {
+        return withContext(Dispatchers.Main) {
             block()
         }
-    }
-
-    /**
-     * Cancel all
-     * 取消所有
-     */
-    @JvmStatic
-    fun cancelAll() {
-        ioScope.cancel()
-        mainScope.cancel()
     }
 }
