6bb89d4b98c49c629d4c7f310599d556c216c736
SilverIceKey <z516798599@gmail.com>
2024-07-12 11:10:57 +0800

    样例代码删除，打包失败预修复提交

--- app/src/main/java/com/sik/skextensionsample/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.sik.skextensionsample
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel: ViewModel() {
-
-}
--- app/src/main/java/com/sik/skextensionsample/ScanConfiguration.kt
+++
@@ -1,6 +0,0 @@
-package com.sik.skextensionsample
-
-import com.sik.sikcore.explain.ScanConfig
-
-@ScanConfig(ObjectDemo::class)
-class ScanConfiguration
