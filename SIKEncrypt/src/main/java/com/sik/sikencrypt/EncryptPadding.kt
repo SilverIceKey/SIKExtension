55d0a55c9ba760c376f305661905e787d0bc451a
周文健 <z516798599@qq.com>
2025-07-25 10:40:54 +0800

    feat(SIKEncrypt): Add PKCS7Padding to EncryptPadding
    This commit introduces `PKCS7Padding` as an option in the `EncryptPadding` enum.
    
    The project version in `gradle.properties` has also been incremented to 1.1.62.

--- SIKEncrypt/src/main/java/com/sik/sikencrypt/EncryptPadding.kt
+++ SIKEncrypt/src/main/java/com/sik/sikencrypt/EncryptPadding.kt
@@ -7,5 +7,6 @@ package com.sik.sikencrypt
 enum class EncryptPadding(val padding: String) {
     NoPadding("NoPadding"),
     PKCS5Padding("PKCS5Padding"),
+    PKCS7Padding("PKCS7Padding"),
     OAEPWithSHA256AndMGF1Padding("OAEPWithSHA-256AndMGF1Padding"),
 }
